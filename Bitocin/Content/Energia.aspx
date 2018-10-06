c9c653dc16a6aa1dccb6e305881dc7263b2e00f5
Theilor Raddatz <theilor@gmail.com>
2018-10-06 11:59:58 -0300

    Cadastro de cidades funcioando

---
+++ Bitocin/Content/Energia.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Energia.aspx.cs" Inherits="Bitocin.Content.Energia" EnableViewState="false" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Crypto Miner Analytics - Energia</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link href="CSS/Bootstrap.css" rel="stylesheet" />
+    <link href="CSS/estilo.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+        <div class="logotipo">
+            <img src="images/logo.png" />
+        </div>
+        <div class="navigation-bar col-md-10">
+            <ul>
+                <li><a href="Index.aspx" data-toggle="tab">Home</a>
+                </li>
+                <li><a href="Comparativo.aspx" data-toggle="tab">Comparativo</a>
+                </li>
+                <li><a href="Moedas.aspx" data-toggle="tab">Moedas</a>
+                </li>
+                <li><a href="Hardwares.aspx" data-toggle="tab">Hardwares</a>
+                </li>
+                <li><a href="Energia.aspx" data-toggle="tab">Energia</a>
+                </li>
+                <li><a href="Faq.aspx" data-toggle="tab">FAQ</a>
+                </li>
+                <li><a href="Sobre.aspx" data-toggle="tab">Sobre</a>
+                </li>
+                </ul>
+        </div>
+        <br />
+        <br />
+        <form id="form1" runat="server">
+            <div>
+            </div>
+            <div class="background-branco">
+                <asp:GridView ID="GridView2" runat="server" class="table table-striped table-bordered table-hover table-condensed" AutoGenerateColumns="false">
+                    <Columns>
+                        <asp:BoundField DataField="cidade"
+                            HeaderText="Cidade" />
+                        <asp:BoundField DataField="estado"
+                            HeaderText="Estado" />
+                        <asp:BoundField DataField="concessionaria"
+                            HeaderText="Concessionária" /> 
+                        <asp:BoundField DataField="custoKWh"
+                            HeaderText="Custo por KW/h" />
+                        <asp:BoundField DataField="emissaoCO2porWh"
+                            HeaderText="CO2/Wh" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+            <br />
+            <br />
+            <h3>Cadastre uma nova cidade:</h3>
+            <div class="col-md-6">
+                <input type="text" class="form-control col-md-7" id="cidade" name="cidade" placeholder="Cidade" />
+                <input type="text" class="form-control col-md-7" id="estado" name="estado" placeholder="UF" />
+                <input type="text" class="form-control col-md-7" id="concessionaria" name="concessionaria" placeholder="Concessionária" /> 
+                <input type="number" class="form-control col-md-7 inlineClass" id="custo" name="custo" placeholder="Custo por KW/h" pattern="[0-9]+([,\.][0-9]+)?" min="0" step="any" /> Reais<br />
+                <input type="number" class="form-control col-md-7 inlineClass" id="co2" name="co2" pattern="[0-9]+([,\.][0-9]+)?" min="0" step="any" placeholder="CO2/Wh" /><br />
+                <asp:Button ID="ButtonCadastro" Text="Cadastrar" OnClick="ButtonCadastro_Click" runat="server" />
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
+<script>
+   <%-- function Recupera() { alert('<%= valor %>'); }--%>
+    function f1() {
+        
+          form1.submit();
+      }
+</script>
---
+++ Bitocin/Content/Energia.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace Bitocin.Content {
+    public partial class Energia : System.Web.UI.Page {
+        public string valor = "Bitcoin";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GeraTabelaCidades();
+        }
+
+        
+        //public string rodaQuery()
+        //{
+        //    string sql = " SELECT senha FROM usuarios  ";
+        //    MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=twitter_clone;SslMode=none");
+        //    MySqlCommand cmd = new MySqlCommand(sql, con);
+        //    string temp = "";
+        //    con.Open();
+
+        //    MySqlDataReader reader = cmd.ExecuteReader();
+
+        //    while (reader.Read())
+        //    {
+        //        temp = reader.GetString("senha");
+
+        //    }
+        //    con.Close();
+        //    return temp;
+        //}
+
+       
+        public void GeraTabelaCidades()
+        {
+            string moeda = "";
+
+            if (Request.Form["selectMoeda"] == null)
+                moeda = "Bitcoin";
+            else
+                moeda = Request.Form["selectMoeda"];
+
+            MySqlConnection SQL_conection = new MySqlConnection("host=localhost;user=root;password='';database=cripto;SslMode=none");
+            String name_tabel = "hardwares";
+            MySqlDataAdapter db_select;
+            DataSet db_data;
+
+            try
+            {
+                db_select = new MySqlDataAdapter("SELECT * FROM custoenergia;", SQL_conection);
+                db_data = new System.Data.DataSet();
+                db_select.Fill(db_data, name_tabel);
+                GridView2.DataSource = db_data;
+                GridView2.DataBind();
+              //  string unidade = db_data.Tables[0].Rows[0]["unidade"].ToString();
+             //   GridView2.Columns[3].HeaderText = unidade;
+                SQL_conection.Close();
+
+                valor = moeda;
+            }
+            catch (Exception e){
+                valor = "Erro ao gerar tabela:" + e.Message;
+            }
+        }
+
+        public void loadTableButton_Click(Object sender, EventArgs e)
+        {
+            GeraTabelaCidades();
+        }
+
+
+        public void ButtonCadastro_Click(Object sender, EventArgs e)
+        {
+            MySqlConnection SQL_conection = new MySqlConnection("host=localhost;user=root;password='';database=cripto;SslMode=none");
+            String name_tabel = "custoenergia";
+
+            string cidade = Request.Form["cidade"];
+            string estado = Request.Form["estado"];
+            string concessionaria = Request.Form["concessionaria"];
+            string custo = Request.Form["custo"];
+            string co2 = Request.Form["co2"];
+
+            try
+            {
+                SQL_conection.Open();
+                MySqlCommand cmd = new MySqlCommand($"INSERT INTO {name_tabel} (cidade,estado,pais,concessionaria,custoKWh,emissaoCO2porWh) VALUES ('{cidade}','{estado}','Brasil','{concessionaria}','{custo}','{co2}');", SQL_conection);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                SQL_conection.Close();
+
+                Page.ClientScript.RegisterStartupScript(GetType(), "MyKey", "alert('Cidade cadastrada com sucesso');", true);
+                GeraTabelaCidades();
+            }
+            catch (Exception e2)
+            {
+                Page.ClientScript.RegisterStartupScript(GetType(), "MyKey", $"alert('Erro ao cadastrar. Por favor, preencha todos os campos.\nErro: {e2.Message}');", true);
+            }
+
+
+        }
+
+    }
+}
+
